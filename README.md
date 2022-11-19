38a11040eb048eb0a0267f102e99e05ee800eb9b
neo-the-coder <kamranz150@gmail.com>
2022-11-20 00:25:38 +0400

    Final state

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # This project is from TOR Etch-A-Sketch
+Grid ontainer and children are styled and create using javascript.
--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 const container = document.getElementById('container');
+container.style.cssText = "min-width: 500px; min-height: 500px; display: grid;";
 
-for (let i = 1; i <= 16*16; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', i);
-    container.appendChild(div);
-    div.addEventListener('mouseenter', event => event.target.style.backgroundColor = '#c0c0c0');
-    div.addEventListener('mouseleave', event => event.target.style.backgroundColor = '#fff');
+function setGrid() {
+    gridx = prompt("How many cells in a row?")
+    if (gridx < 1 || gridx > 100) {
+        alert("Value must be between 1 and 100. Try again");
+        return;
+    } else if (/\D/.test(gridx)) {alert("Not a number! Try again"); return;}
 
+    gridy = prompt("How many cells in a column?");
+    if (gridy < 1 || gridx > 100) {
+        alert("Value must be between 1 and 100. Try again");
+        return;
+    } else if (/\D/.test(gridy)) {alert("Not a number! Try again"); return;}
+
+    buildGrid(gridx, gridy);
 }
+
+function buildGrid(gridx=16, gridy=16) {
+
+    //Remove #container's all children
+    container.innerHTML = "";
+    //Set columns according to given value
+    container.style.gridTemplateColumns = "repeat(" + gridx + ", 1fr)";
+
+    //Create given number of divs, add border and events using loop 
+    for (let i = 1; i <= gridx*gridy; i++) {
+        const div = document.createElement('div');
+        // div.setAttribute('id', i);
+        div.style.border = '1px solid grey';
+        container.appendChild(div);
+        div.addEventListener('mouseenter', event => event.target.style.backgroundColor = '#c0c0c0');
+        div.addEventListener('mouseleave', event => event.target.style.backgroundColor = 'rgba(0,0,0,0)');
+    }
+}
+
+// Building grid with default values, i.e. 16x16
+buildGrid();
