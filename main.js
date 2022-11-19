8f33cbaf2e225ff51ea308eae06ad1fbdaf604db
neo-the-coder <kamranz150@gmail.com>
2022-11-19 17:27:30 +0400

    Change css filename to style.css, add hover effect using js

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ for (let i = 1; i <= 16*16; i++) {
     const div = document.createElement('div');
     div.setAttribute('id', i);
     container.appendChild(div);
+    div.addEventListener('mouseenter', event => event.target.style.backgroundColor = '#c0c0c0');
+    div.addEventListener('mouseleave', event => event.target.style.backgroundColor = '#fff');
+
 }
